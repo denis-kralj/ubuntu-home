5e6d5a0f452ed713e8819323c81534a2290576a0
Denis Kralj <denis-kralj@users.noreply.github.com>
2022-03-12 17:36:40 +0100

    Change executablility of file

--- get-programs.sh
+++ get-programs.sh
@@ -7,7 +7,7 @@ sudo apt-get install zsh awscli jq
 sudo chsh -s $(which zsh)
 
 # download and install oh my zsh
-$ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # download and install oh my posh
 sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
